cfd06ec256dcd1af70c2d40d241d05ec0fc16a24
atleyvarghese <atleyvarghese@gmail.com>
2017-10-06 18:25:21 +0530

    first 7

--- heroes/settings.py
+++ heroes/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsPostCsrfMiddleware',
 
 ]
 
