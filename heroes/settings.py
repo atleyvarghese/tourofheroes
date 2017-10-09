f6f312c4bbed4c31fdc87c36b3f8d8a3cc5eed01
atleyvarghese <atleyvarghese@gmail.com>
2017-10-09 11:50:39 +0530

    final 3

--- heroes/settings.py
+++ heroes/settings.py
@@ -155,8 +155,10 @@ ACCOUNT_LOGOUT_ON_GET = True
 SITE_ID = 1
 
 
-# CORS_ORIGIN_WHITELIST =
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+'tourofheroes-atley.herokuapp.com',
+)
+# CORS_ORIGIN_ALLOW_ALL = True
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
